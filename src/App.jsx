4f4af40983495cce88292cc872e18f2a246219fc
Senimtra <senimtra@hotmail.com>
2021-11-30 23:10:30 +0100

    update colors create /styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.scss";
+import "./styles/main.scss";
 
 function App() {
-  return <div className="App">Portfolio-page</div>;
+	return (
+		<div className="App">
+			<span id="soon">soon</span>
+			<span id="tm">™</span>
+		</div>
+	);
 }
 
 export default App;

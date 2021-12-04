f76db1d4c2c71af4d82bbc703a83873695cc66e8
Senimtra <senimtra@hotmail.com>
2021-12-04 22:23:46 +0100

    add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-md bg-transparent">
+			<div className="container-fluid">
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="/"
+								id="navbarDropdown"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+							>
+								Labs
+							</a>
+							<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li>
+									<a className="dropdown-item" href="/">
+										Lab 1
+									</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="/">
+										Lab 2
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;

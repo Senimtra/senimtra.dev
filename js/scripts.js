bce9d67ef75cf57664c9f458e6c7a40edded7073
Senimtra <senimtra@hotmail.com>
2022-06-18 14:43:32 +0200

    link nav event listeners to css classes

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,26 @@
 // ##  Navbar positioning  ##
 // ##########################
 
-const navHome = () => {
-	console.log('nav-home');
-};
-
-const navGeneral = () => {
-	console.log('nav-general');
-};
-
-// ###  Event listeners ###
-
+// ### Event listeners ###
 document.querySelector('.nav-home').addEventListener('click', () => navHome());
 document
 	.querySelectorAll('.nav-general')
 	.forEach(el => el.addEventListener('click', () => navGeneral()));
+
+// ### Link home nav style (center) ###
+const navHome = () => {
+	const navbar = document.querySelector('.navbar');
+	if (navbar.classList.contains('nav-center')) {
+		navbar.classList.remove('nav-center');
+		navbar.classList.add('nav-top');
+	}
+};
+
+// ### Link general nav style (top) ###
+const navGeneral = () => {
+	const navbar = document.querySelector('.navbar');
+	if (navbar.classList.contains('nav-top')) {
+		navbar.classList.remove('nav-top');
+		navbar.classList.add('nav-center');
+	}
+};

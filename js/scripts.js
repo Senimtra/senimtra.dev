4101d42874240fe1da02e4fef80e08de5d704b9f
Senimtra <senimtra@hotmail.com>
2022-06-19 15:44:59 +0200

    add basic navbar styles

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,17 @@ document
 // ### Link home nav style (center) ###
 const navHome = () => {
 	const navbar = document.querySelector('.navbar');
-	if (navbar.classList.contains('nav-center')) {
-		navbar.classList.remove('nav-center');
-		navbar.classList.add('nav-top');
+	if (navbar.classList.contains('nav-top')) {
+		navbar.classList.remove('nav-top');
+		navbar.classList.add('nav-center');
 	}
 };
 
 // ### Link general nav style (top) ###
 const navGeneral = () => {
 	const navbar = document.querySelector('.navbar');
-	if (navbar.classList.contains('nav-top')) {
-		navbar.classList.remove('nav-top');
-		navbar.classList.add('nav-center');
+	if (navbar.classList.contains('nav-center')) {
+		navbar.classList.remove('nav-center');
+		navbar.classList.add('nav-top');
 	}
 };

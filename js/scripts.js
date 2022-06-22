f49ba06b9871849f877698674017e5379f764dee
Senimtra <senimtra@hotmail.com>
2022-06-22 19:04:40 +0200

    optimize navbar dom manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,12 @@
 // ##  Navbar positioning  ##
 // ##########################
 
-window.onload = () => {
-	navPosition();
-};
+window.onload = () => navPosition();
 
 window.onresize = () => {
 	// ### Onresize performance timeout ###
 	clearTimeout(window.resizeTimeout);
-	window.resizeTimeout = setTimeout(() => {
-		navPosition();
-	}, 100);
-};
-
-const navPosition = () => {
-	const navCenter = document.querySelector('.nav-center');
-	const winWidth = window.innerWidth;
-	// ### Navbar horizontal positioning ###
-	if (navCenter && winWidth < 992) {
-		const navWidth = navCenter.clientWidth;
-		const navPosX = `${(winWidth - navWidth) / 2}px`;
-		navCenter.style.left = (winWidth > navWidth && navPosX) || '0';
-	} else {
-		navCenter.style.left = `${(winWidth / 100) * 15}px`;
-	}
-	// ### Navbar vertical positioning ###
-	if (navCenter) {
-		const navHeight = navCenter.clientHeight;
-		const winHeight = window.innerHeight;
-		const navPosY = `${(winHeight - navHeight) / 2}px`;
-		navCenter.style.top = (winHeight > navHeight && navPosY) || '0';
-	}
+	window.resizeTimeout = setTimeout(() => navPosition(), 100);
 };
 
 // ### Event listeners ###
@@ -40,22 +16,40 @@ document
 	.querySelectorAll('.nav-general')
 	.forEach(el => el.addEventListener('click', () => navGeneral()));
 
+const navbarElement = document.querySelector('.navbar');
+const navCenterElement = document.querySelector('.nav-center');
+
 // ### Link home nav style (center) ###
 const navHome = () => {
-	const navbar = document.querySelector('.navbar');
-	if (navbar.classList.contains('nav-top')) {
-		navbar.classList.remove('nav-top');
-		navbar.classList.add('nav-center');
-		window.onresize();
+	if (navbarElement.classList.contains('nav-top')) {
+		navbarElement.classList.replace('nav-top', 'nav-center');
+		navPosition();
 	}
 };
 
 // ### Link general nav style (top) ###
 const navGeneral = () => {
-	const navbar = document.querySelector('.navbar');
-	if (navbar.classList.contains('nav-center')) {
-		navbar.classList.remove('nav-center');
-		navbar.classList.add('nav-top');
-		navbar.style.top = '0';
+	if (navbarElement.classList.contains('nav-center')) {
+		navbarElement.classList.replace('nav-center', 'nav-top');
+		navbarElement.style.top = '0';
+	}
+};
+
+const navPosition = () => {
+	const winWidth = window.innerWidth;
+	// ### Navbar horizontal positioning ###
+	if (navCenterElement && winWidth < 992) {
+		const navWidth = navCenterElement.clientWidth;
+		const navPosX = `${(winWidth - navWidth) / 2}px`;
+		navCenterElement.style.left = (winWidth > navWidth && navPosX) || '0';
+	} else {
+		navCenterElement.style.left = `${(winWidth / 100) * 15}px`;
+	}
+	// ### Navbar vertical positioning ###
+	if (navCenterElement) {
+		const navHeight = navCenterElement.clientHeight;
+		const winHeight = window.innerHeight;
+		const navPosY = `${(winHeight - navHeight) / 2}px`;
+		navCenterElement.style.top = (winHeight > navHeight && navPosY) || '0';
 	}
 };
